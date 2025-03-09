2eb479e82bb1148dd2760a2a9445ceca833c823c
Maxim D <renigano@gmail.com>
2025-03-09 21:36:06 +0300

    feat: fix

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from 'express'
+import express from 'express'
 import cors from 'cors'
 import {SETTINGS} from "./settings";
 import {testRouter} from "./test/testController";
@@ -8,10 +8,6 @@ export const app = express()
 app.use(express.json())
 app.use(cors())
 
-
-app.delete('/testing/all-data',  (req: Request, res: Response) => {
-    res.status(200).json({asd:'asd'})
-})
 
 app.use(SETTINGS.PATH.VIDEOS, videoRouter)
 app.use(SETTINGS.PATH.TEST, testRouter)
--- src/videos/videoController.ts
+++ src/videos/videoController.ts
@@ -16,7 +16,6 @@ export const videoController = {
     createVideo: (req: Request<ParamType, OutputType, NewVideoBodyType, QueryType>, res: Response) => {
         const errorsMessages: ErrorType[] = []
         titleValidation(req.body.title, errorsMessages)
-
         if(errorsMessages.length > 0){
             res.status(400).json({errors: errorsMessages}).end()
             return
