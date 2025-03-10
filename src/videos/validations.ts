7d67d5bf9e420c0a936395b803fe6486205c9e24
Maxim D <renigano@gmail.com>
2025-03-10 20:44:10 +0300

    fix: fix update req

--- src/videos/validations.ts
+++ src/videos/validations.ts
@@ -32,7 +32,10 @@ export const availableResolutionsValidation = (availableResolutions: string[], e
 
 }
 export const minAgeRestrictionValidation = (age: number, errors: ErrorType[]) => {
-    if(age <1 || age > 18) {
+    if(age <1) {
+        errors.push({message: 'error', field: 'minAgeRestriction'})
+    }
+    if(age > 18) {
         errors.push({message: 'error', field: 'minAgeRestriction'})
     }
 
